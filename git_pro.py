916f0618b383334fbc92753c6de2fa858f5cde76
nniinima <niko.niinimaki@gmail.com>
2021-07-05 14:16:44 +0300

    Person class added

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,22 @@
 def hello():
     return "Hello World!"
+
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, x):
+        self._name = x
+
+    def set_age(self, x):
+        self._age = x
+
+    def __str__(self):
+            return f'name: {self._name}, age: {self._age}'
